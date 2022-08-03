72a4afd0f76294529337c6253e95bfe0be0f53c1
Justin Fuller <justin.93x@gmail.com>
2022-08-03 15:44:49 -0500

    App Initialization

---
+++ slack-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCE0xjE7jTfXrYhPJHmu08UBgo3O4YW6Aw",
+  authDomain: "slack-clone-5cd38.firebaseapp.com",
+  projectId: "slack-clone-5cd38",
+  storageBucket: "slack-clone-5cd38.appspot.com",
+  messagingSenderId: "162846358667",
+  appId: "1:162846358667:web:79fee366afabb7d3de688e",
+};
